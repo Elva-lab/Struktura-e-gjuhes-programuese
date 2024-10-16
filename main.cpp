4f0090e384718bb8c4730f08c1958ef1818e6f01
Elva-lab <elva.hamiti@gmail.com>
2024-10-16 12:40:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Gjithmone komentet shenohen me identifikatori //
+// Komentet me // jane komente brenda nje rreshti
+// Komentet me /* */ jane komente brenda nje paragrafi
+
+/*
+Cfardo qe shenojme ketu eshte koment...
+edhe ketu...
+edhe ketu
+*/
+
+#include <iostream> //Kjo tregon qe do te importojme biblioteken e C++ (Input-Output stream)
+using namespace std; // Kjo tregon qe do perdorim namespace si std... shkurtese
+
+int main() // funksioni main() eshte funksioni i pare dhe kryesor i programit
+{          // Kjo eshte nje blok i kodit qe do te kryejne nga ky funksion
+
+  // cfardo qe shenojm ene brendi te main funksionit do te konsiderohet si
+  // ekzekutim
+
+  // komandat dhe fjalet e rezervuara zakonisht kane ngjyrime ne varesi te IDE
+  // qe e perdorim...
+
+  cout << "Hello World!\n"; // Komandat cout do te printojme nje tekst ne ekran
+
+} // Ketu do te kthehet ky funksioni main()
